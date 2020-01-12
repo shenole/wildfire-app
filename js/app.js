33578a4eff2cb63f69b7abaf176ff7b357cdc3e4
Shenole Latimer <shenole@gmail.com>
2020-01-11 20:54:01 -0500

    flame icon is animated and map api is also being accessed

---
+++ js/app.js
@@ -0,0 +1,19 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2hhcnA5bWVkaWEiLCJhIjoiY2s1YTl0bTVqMGc2djNucGRxdmxuYzRuNiJ9.-5J7mOB7O3RM-lAC3CoLog';
+let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: [-103.59179687498357, 40.66995747013945],
+    zoom: 1
+});
+
+// let long = 
+// let lat = 
+// let zoomMap = 
+
+// let map = new mapboxgl.Map({
+//     container: 'map',
+//     style: 'mapbox://styles/mapbox/streets-v9',
+//     center: [-73.4257, 40.8682],
+//     zoom: 12
+// });
+
