fd3e6f1e2285306382c828fbf1fe926662fd1423
Shenole Latimer <shenole@gmail.com>
2020-01-12 00:35:03 -0500

    form and map functionality is complete. Next is to pull in weather data from weather api

--- js/app.js
+++ js/app.js
@@ -6,14 +6,78 @@ let map = new mapboxgl.Map({
     zoom: 1
 });
 
-// let long = 
-// let lat = 
-// let zoomMap = 
+// GLOBAL VARIABLES
 
-// let map = new mapboxgl.Map({
-//     container: 'map',
-//     style: 'mapbox://styles/mapbox/streets-v9',
-//     center: [-73.4257, 40.8682],
-//     zoom: 12
-// });
+let form = document.querySelector('#location-form');
+let lat = document.querySelector('#latitude');
+let long = document.querySelector('#longitude');
 
+// FUNCTION THAT UPDATES MAP
+
+let updateMap = () => {
+    let newLong = long.value;
+    let newLat = lat.value;
+    let map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9',
+        center: [newLong, newLat],
+        zoom: 12
+    });
+    // Marker is added to the new map
+    map.on('load', function () {
+        map.loadImage(
+            'https://upload.wikimedia.org/wikipedia/commons/0/0a/Marker_location.png',
+            function (error, image) {
+                if (error) throw error;
+                map.addImage('cat', image);
+                map.addLayer({
+                    'id': 'points',
+                    'type': 'symbol',
+                    'source': {
+                        'type': 'geojson',
+                        'data': {
+                            'type': 'FeatureCollection',
+                            'features': [
+                                {
+                                    'type': 'Feature',
+                                    'geometry': {
+                                        'type': 'Point',
+                                        'coordinates': [newLong, newLat]
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    'layout': {
+                        'icon-image': 'cat',
+                        'icon-size': 1.0
+                    }
+                });
+            }
+        );
+    });
+}
+
+// CLICK SUBMIT BUTTON LOADS NEW LOCATION, UPDATES WEATHER, AND PROVIDES INDEX VALUES
+
+let submitForm = (e) => {
+    e.preventDefault();
+    updateMap();
+}
+
+document.querySelector('#submit-btn').addEventListener('click', submitForm);
+
+// CLICK RESET BUTTON RESETS LATITUDE AND LONGITUDE FIELDS TO BLANK, LOADS DEFAULT MAP, CLEARS WEATHER, AND CLEARS INDEX VALUES
+
+let resetForm = (e) => {
+    e.preventDefault();
+    form.reset();
+    let map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9',
+        center: [-103.59179687498357, 40.66995747013945],
+        zoom: 1
+    });
+}
+
+document.querySelector('#reset-btn').addEventListener('click', resetForm);
